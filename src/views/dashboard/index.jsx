2342efdfa08cfa56d34abe5eba9328459e2f8190
victorCORREIA14 <augustoalves.sep@gmail.com>
2024-08-01 01:33:05 -0300

    fiz controle de acesso

--- src/views/dashboard/index.jsx
+++ src/views/dashboard/index.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import Storage from "local-storage";
 
 export const DashboardView = () => {
+  const navigateTo = useNavigate();
+
+  useEffect(() => {
+    if (Storage("@user") == null) {
+      navigateTo("/");
+    }
+  }, []);
+
   return (
     <div>
       <h1>Dashboard</h1>
     </div>
-  )
-}
+  );
+};
